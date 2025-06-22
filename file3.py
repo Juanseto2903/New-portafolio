4139f84c2d7514a70de950df035a1056d0081745
Juans3to <juanseto36@gmail.com>
2025-06-22 11:55:17 -0500

    Lacking name added

--- file3.py
+++ file3.py
@@ -5,8 +5,8 @@ random.seed(40)
 
 dic = {
 
-    'Name': ['Juan', 'Maria', 'xd'],
-    'Age': [random.randint(18,30) for _ in range(3)]
+    'Name': ['Juan', 'Maria', 'xd', 'Santiago'],
+    'Age': [random.randint(18,30) for _ in range(4)]
 }
 
 df = pd.DataFrame(dic)
